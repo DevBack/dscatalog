3fd7e6802f55230d769721743f965d2e172929ab
DevBack <heltonlima.ti@live.com>
2023-03-17 17:20:13 -0300

    Backend Project Created

---
+++ backend/src/test/java/com/devback/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devback.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
