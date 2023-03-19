cd8df08573dd06858273dbdae369a580f30125fd
DevBack <heltonlima.ti@live.com>
2023-03-19 10:47:40 -0300

    H2 Database

---
+++ backend/src/main/java/com/devback/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.devback.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devback.dscatalog.entities.Category;
+import com.devback.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
