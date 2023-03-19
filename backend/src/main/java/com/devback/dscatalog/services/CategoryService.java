5377373b6ec4194807376c31b3c7ce10e99a36c0
DevBack <heltonlima.ti@live.com>
2023-03-19 11:55:45 -0300

    DTO

--- backend/src/main/java/com/devback/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devback/dscatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.devback.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devback.dscatalog.dto.CategoryDTO;
 import com.devback.dscatalog.entities.Category;
 import com.devback.dscatalog.repositories.CategoryRepository;
 
@@ -14,7 +16,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();	
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
