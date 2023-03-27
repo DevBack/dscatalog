da0cfcc160407988eb6f0c347bda8bf740423fb2
DevBack <heltonlima.ti@live.com>
2023-03-27 13:19:59 -0300

    Pagination

--- backend/src/main/java/com/devback/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devback/dscatalog/services/CategoryService.java
@@ -1,11 +1,10 @@
 package com.devback.dscatalog.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,9 +23,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> list = repository.findAll();	
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
@@ -55,6 +54,7 @@ public class CategoryService {
 		}
 	}
 
+	@Transactional
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
@@ -66,6 +66,5 @@ public class CategoryService {
 			throw new DatabaseException("Integrity Violation");
 		}
 	}
-	
 	
 }
